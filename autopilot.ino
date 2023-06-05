aa821288c34819b5f474fd460e6294d483d31c4a
Farzan Ali Faisal <farzanalifaisal@Farzans-MBP.ht.home>
2023-06-05 16:06:19 -0400

    reading acceleration values

--- autopilot.ino
+++ autopilot.ino
@@ -11,26 +11,28 @@
 
 Servo elevator;
 int elevatorPin = 10;
-
-MPU6050 mpu;
-int16_t ax, ay, az;
-int16_t gx, gy, gz;
-
-struct MyData {
-  byte X;
-  byte Y;
-  byte Z;
-};
-
-MyData data;
-
 float pulse;
+float accX, accY, accZ;
+
+//Neutral servo positionings
+int neutralElev = 120;
+
 void setup() {
   Serial.begin(9600);
   pinMode(9, INPUT);
   pinMode(13, OUTPUT);
   Wire.begin();
-  mpu.initialize();
+}
+
+void autopilotOn() {
+  elevator.attach(elevatorPin);
+  // Moving the servo
+  // elevator.write(30);
+}
+
+void autopilotOff() {
+  pinMode(elevatorPin, LOW);
+  elevator.detach();
 }
 
 void loop() {
@@ -38,39 +40,27 @@ void loop() {
   if (pulse > 1.8) {
     digitalWrite(13, HIGH);
     autopilotOn();
-    printGyroData();
+
+    Wire.beginTransmission(0x68);
+    Wire.write(0x3B);
+    Wire.endTransmission(false);
+    Wire.requestFrom(0x68, 6, true);
+
+    accX = Wire.read()<<8|Wire.read();
+    accY = Wire.read()<<8|Wire.read();
+    accZ = Wire.read()<<8|Wire.read();
+    accX = (float)accX/16384.0;
+    accY = (float)accY/16384.0;
+    accZ = (float)accZ/16384.0;
+
+    Serial.print(accX);
+    Serial.print(" ");
+    Serial.print(accY);
+    Serial.print(" ");
+    Serial.println(accZ);
+    delay(500);
   } else {
     digitalWrite(13, LOW);
     autopilotOff();
   }
-}
-
-void autopilotOn() {
-  elevator.attach(elevatorPin);
-  // Moving the servo
-  elevator.write(30);
-}
-
-void autopilotOff() {
-  //move servo back to neutral positioning
-  elevator.write(90);
-  // delay(100);
-  pinMode(10, LOW);
-  elevator.detach();
-}
-
-void printGyroData() {
-  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-  data.X = map(ax, -17000, 17000, 0, 255);  // X axis data
-  data.Y = map(ay, -17000, 17000, 0, 255);
-  data.Z = map(az, -17000, 17000, 0, 255);  // Y axis data
-  delay(100);
-  Serial.print("Axis X = ");
-  Serial.print(data.X);
-  Serial.print("  ");
-  Serial.print("Axis Y = ");
-  Serial.print(data.Y);
-  Serial.print("  ");
-  Serial.print("Axis Z  = ");
-  Serial.println(data.Z);
 }
